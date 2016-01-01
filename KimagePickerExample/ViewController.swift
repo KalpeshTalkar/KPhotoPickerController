b1d4ddd8a043d95b9271ac6b1eb5bb5e4de20b91
KalpeshTalkar <kalpeshtalkar@gmail.com>
2016-01-01 20:39:54 +0530

    Added collection view to see all the selected photos

--- KimagePickerExample/ViewController.swift
+++ KimagePickerExample/ViewController.swift
@@ -8,21 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController, KPhotoPickerDelegate {
+class ViewController: UIViewController, KPhotoPickerDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var label: UILabel!
     @IBOutlet weak var selectPhotosBtn: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    var photosArray = Array<UIImage>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        label.text = "Photos selected: 0"
+
         selectPhotosBtn.layer.cornerRadius = 3
         selectPhotosBtn.layer.shadowColor = UIColor.darkGrayColor().CGColor
         selectPhotosBtn.layer.shadowOffset = CGSizeMake(0, 1)
         selectPhotosBtn.layer.shadowOpacity =  0.5
         selectPhotosBtn.layer.shadowRadius = 3
         
+        collectionView.backgroundColor = UIColor.clearColor()
     }
 
+    // MARK - Button Action
     @IBAction func selectPhotos(sender: UIButton) {
         // Initialize photo picker controller
         let photoPicker = KPhotoPickerController.init()
@@ -34,8 +41,36 @@ class ViewController: UIViewController, KPhotoPickerDelegate {
     
     // MARK - KPhotoPickerControllerDelegate
     func kPhotoPickerController(didFinishSelectingPhotos photos: [UIImage]) {
-        print("total pics selected: \(photos.count)")
+        label.text = "Photos selected: \(photos.count)"
+        photosArray = photos
+        collectionView.reloadData()
     }
 
+    // MARK - CollectionView Data Source
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photosArray.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath) as! PictureCell
+        cell.layer.masksToBounds = false
+        cell.layer.cornerRadius = 5
+        cell.layer.shadowColor = UIColor.darkGrayColor().CGColor
+        cell.layer.shadowOffset = CGSizeMake(0, 1)
+        cell.layer.shadowOpacity = 0.5
+        cell.layer.shadowRadius = 5
+        cell.imageView.clipsToBounds = true
+        cell.imageView.layer.cornerRadius = 5
+        cell.imageView.image = photosArray[indexPath.item];
+        return cell;
+    }
+}
+
+class PictureCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
 }
 
